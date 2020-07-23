0acd8273768e7e68c996916101ffb1a9354673d3
Richard Lawrence <richardlaw192@gmail.com>
2020-07-23 09:24:29 +0100

    adding gaspra.scheduler.common

---
+++ src/Gaspra.Scheduler.Common/IScheduledJob.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Gaspra.Scheduler.Common
+{
+    public interface IScheduledJob
+    {
+        public IDictionary<string, object> Context { get; set; }
+        public void Execute();
+    }
+}
