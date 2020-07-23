50e90f970c1cac029bef61b4396b235a14335d3f
Richard Lawrence <richardlaw192@gmail.com>
2020-07-23 16:52:36 +0100

    add async scheduled job

---
+++ src/Gaspra.Scheduler.Common/IAsyncScheduledJob.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gaspra.Scheduler.Common
+{
+    public interface IAsyncScheduledJob
+    {
+        public IDictionary<string, object> Context { get; set; }
+        public Task Execute();
+    }
+}
